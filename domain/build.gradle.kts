00a4c9a1416fb8503a4172108ffa5e8b9b4c88aa
Igor <i.nechaev@lipt-soft.ru>
2022-02-05 18:24:52 +0300

    add domain skeleton

---
+++ domain/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    kotlin("kapt")
+}
+
+android {
+
+    compileSdk = 32
+
+    defaultConfig {
+        minSdk = 24
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+
+        getByName("release")
+
+        getByName("debug")
+
+    }
+
+}
+
+dependencies{
+    api(project(":core"))
+}
